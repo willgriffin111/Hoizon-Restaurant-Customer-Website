7d05a1cee3d09a63305d1d5bef02f17ce07af9c4
Oscar Linehan <oladmin@Oscars-MacBook-Air.local>
2024-03-23 15:09:59 +0000

    Added rooting, started flash notifications for errors

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Alex Rogers22018703
 import string
 import mysql.connector
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 from passlib.hash import sha256_crypt
 import hashlib
 import gc
@@ -32,116 +32,72 @@ def reservationspage():
     return render_template('userReservations.html')
 
 
-@app.route('/login', methods=["GET","POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    form={}
+    form = {}
     error = ''
-    try:	
-        if request.method == "POST":            
-            email = request.form['email']
-            password = request.form['password']            
-            form = request.form
-            print('login start 1.1')
-            
-            if email != None and password != None:  #check if em or pw is none          
-                conn = dbfunc.getConnection() 
-                if conn != None:    #Checking if connection is None                    
-                    if conn.is_connected(): #Checking if connection is established                        
-                        print('MySQL Connection is established')                          
-                        dbcursor = conn.cursor()    #Creating cursor object                                                 
-                        dbcursor.execute("SELECT User_Password, user_type, User_Firstname, User_ID \
-                            FROM user_data WHERE User_Email = %s;", (email,))                                                
+    try:
+        if request.method == "POST":
+            email = request.form.get('email')
+            password = request.form.get('password')
+            if email and password:  # Simplified the condition
+                conn = dbfunc.getConnection()
+                if conn:
+                    with conn.cursor() as dbcursor:
+                        dbcursor.execute("SELECT User_Password, user_type, User_Firstname, User_ID FROM user_data WHERE User_Email = %s;", (email,))
                         data = dbcursor.fetchone()
-                        #print(data[0])
-                        if dbcursor.rowcount < 1: #this mean no user exists                         
-                            error = "Email / password does not exist, login again"
-                            return render_template("Login.html", error=error, logged_in=session['logged_in'])
-                        else:                            
-                            #data = dbcursor.fetchone()[0] #extracting password   
-                            # verify passowrd hash and password received from user                                                             
-                            if sha256_crypt.verify(request.form['password'], str(data[0])):
-                                dbcursor.close()
-                                conn.close()
-                                gc.collect()   
+                        if data:
+                            if sha256_crypt.verify(password, data[0]):
                                 session['userid'] = int(data[3])
-                                print(session['userid'])                             
-                                session['logged_in'] = True     #set session variables
+                                session['logged_in'] = True
                                 session['username'] = str(data[2])
-                                session['usertype'] = str(data[1])                          
-                                print("You are now logged in")                                
+                                session['usertype'] = str(data[1])
                                 return redirect(url_for('Horizon_Front'))
                             else:
                                 error = "Invalid credentials username/password, try again."
-                                dbcursor.close()
-                                conn.close()
-                                gc.collect()                               
-                    gc.collect()
-                    print('login start 1.10')
-                    return render_template("Login.html", form=form, error=error, logged_in=session['logged_in'])
-    except Exception as e:                
+                        else:
+                            error = "Email / password does not exist, login again"
+                else:
+                    error = "Database connection error"
+    except Exception as e:
         error = str(e) + " <br/> Invalid credentials, try again."
-        return render_template("Login.html", form=form, error = error, logged_in=session['logged_in'])   
-    
-    return render_template("Login.html", form=form, error = error, logged_in=session['logged_in'])
+
+    return render_template("userLogin.html", form=form, error=error, logged_in=session.get('logged_in'))
 
 @app.route('/register', methods=['POST', 'GET'])
-def SignUp():
+def register():
     error = ''
-    print('Register start')
     try:
         if request.method == "POST":
-            email = request.form['email']         
-            Firstname = request.form['Firstname']
-            Seccondname = request.form['Seccondname']
-            password = request.form['password']   
-            print(password,Seccondname,Firstname,email)                 
-            if Firstname != None and Seccondname != None and password != None and email != None:
-                print("sent request")
-                conn = dbfunc.getConnection()           
-                if conn != None:    #Checking if connection is None           
-                    if conn.is_connected(): #Checking if connection is established
-                        print('MySQL Connection is established')                          
-                        dbcursor = conn.cursor()    #Creating cursor object 
-                        #here we should check if username / email already exists                                                           
-                        password = sha256_crypt.hash((str(password)))           
-                        Verify_Query = "SELECT * FROM user_data WHERE User_Email = %s;"
-                        dbcursor.execute(Verify_Query,(email,))
-                        rows = dbcursor.fetchall()           
-                        if dbcursor.rowcount > 0:   #this means there is a user with same name
-                            print('Email already in-use, please use another or log in')
+            email = request.form.get('email')
+            firstname = request.form.get('firstname')  # corrected variable names
+            email = request.form.get('email')  # corrected variable names
+            password = request.form.get('password')
+            if email and firstname and password:  # Simplified the condition
+                conn = dbfunc.getConnection()
+                if conn:
+                    with conn.cursor() as dbcursor:
+                        dbcursor.execute("SELECT * FROM user_data WHERE User_Email = %s;", (email,))
+                        rows = dbcursor.fetchall()
+                        if rows:
                             error = "Email already in-use, please use another or log in"
-                            return render_template("userRegister.html", error=error, logged_in=session['logged_in'])
-                          
-                        else:   #this means we can add new user             
-                            dbcursor.execute("INSERT INTO user_data (User_FirstName, User_LastName, User_Password, \
-                                 User_Email) VALUES (%s, %s, %s, %s)", (Firstname, Seccondname, password, email))                
-                            conn.commit()  #saves data in database              
-                            print("Thanks for registering!")
-                            dbcursor.execute('SELECT User_ID FROM USER_DATA WHERE User_FirstName = %s AND User_LastName = %s AND User_Email = %s;', (Firstname, Seccondname, email))
-                            data = dbcursor.fetchone()
-                            session['userid'] = data[0]
-                            print(session['userid'])
-                            dbcursor.close()
-                            conn.close()
-                            gc.collect()                        
-                            session['logged_in'] = True     #session variables
-                            session['username'] = Firstname
-                            session['usertype'] = 'standard'   #default all users are standard
+                        else:
+                            password_hash = sha256_crypt.hash(password)
+                            dbcursor.execute("INSERT INTO user_data (User_FirstName, User_LastName, User_Password, User_Email) VALUES (%s, %s, %s, %s)", (firstname, secondname, password_hash, email))
+                            conn.commit()
+                            session['logged_in'] = True
+                            session['username'] = firstname
+                            session['usertype'] = 'standard'
                             return redirect(url_for('Horizon_Front'))
-                    else:                        
-                        print('Connection error')
-                        return 'DB Connection Error'
-                else:                    
-                    print('Connection error')
-                    return 'DB Connection Error'
-            else:                
-                print('empty parameters')
-                return render_template("userRegiser.html", error=error, logged_in=session['logged_in'])
-        else:            
-            return render_template("userRegiser.html", error=error, logged_in=session['logged_in'])        
-    except Exception as e:                
-        return render_template("userRegiser.html", error=e, logged_in=session['logged_in'])    
-    return render_template("userRegiser.html", error=error, logged_in=session['logged_in'])
+                else:
+                    error = "Database connection error"
+            else:
+                error = "Incomplete parameters"
+    except Exception as e:
+        error = str(e)
+
+    return render_template("userRegister.html", error=error, logged_in=session.get('logged_in'))
+
 
 @app.route('/resetpassword', methods=["GET","POST"])
 def resetpassword():
@@ -191,18 +147,21 @@ def resetpassword():
 def privacy():
     return render_template('userPolicyPage.html', logged_in=session['logged_in'])
 
+
+
+
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         if 'logged_in' in session:
-            if(session["logged_in"] == True):
+            if session["logged_in"]:
                 return f(*args, **kwargs)
             else:
-                print("You need to login first")
-                return redirect(url_for('Login', error='You need to login first')) 
-        else:            
-            print("You need to login first")
-            return redirect(url_for('Login', error='You need to login first'))   
+                flash('You need to login first', 'error')
+                return redirect(url_for('login'))
+        else:
+            flash('You need to login first', 'error')
+            return redirect(url_for('login'))
     return wrap
 
 @app.route('/account')
